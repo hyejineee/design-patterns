d5049002b161719ee86878c0e9247bd504a61e13
hyeonhyejin <dev.hyejin04@gmail.com>
2019-12-30 15:06:52 +0900

    Make swim function
    Make swim behavior interface.

---
+++ src/main/kotlin/strategy/interfaces/Swim.kt
@@ -0,0 +1,8 @@
+package strategy.interfaces
+
+class Swim : SwimBehavior {
+    override fun swim() {
+        println("헤엄을 친다~~첨벙첨벙")
+    }
+
+}
--- src/test/kotlin/strategy/DuckTest.kt
+++ src/test/kotlin/strategy/DuckTest.kt
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import strategy.interfaces.FlyNoWay
+import strategy.interfaces.Sink
 import strategy.interfaces.Squack
+import strategy.interfaces.Swim
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
@@ -48,5 +50,35 @@ internal class DuckTest {
         duck.performQuack()
 
         assertThat(outContent.toString()).isEqualTo("Can't fly!\n삑삑!\n")
+    }
+
+    @Test
+    fun `SwimDuck`() {
+        class SwimDuck : Duck() {
+            override fun display() {
+                // TODO
+            }
+        }
+
+        val duck = SwimDuck()
+        duck.swimBehavior = Swim()
+        duck.performSwim()
+
+        assertThat(outContent.toString()).isEqualTo("헤엄을 친다~~첨벙첨벙\n")
+    }
+
+    @Test
+    fun `IronDuck`() {
+        class IronDuck : Duck() {
+            override fun display() {
+                // TODO
+            }
+        }
+
+        val duck = IronDuck()
+        duck.swimBehavior = Sink()
+        duck.performSwim()
+
+        assertThat(outContent.toString()).isEqualTo("꼬르르ㅡㅡ륵~~\n")
     }
 }
